735bb44e114bbabd370a34f8862c31d169c00f80
codesmitty <varcodesmitty@gmail.com>
2022-02-19 15:56:32 -0500

    working on auth

--- client/src/hoc/Layout.js
+++ client/src/hoc/Layout.js
@@ -1,35 +1,46 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import useFetch from 'react-fetch-hook';
 import axios from 'axios'
+import {useAuthStore} from '../utility/GlobalState'
 
 function Layout({children}) {
-//  const config = {
-//    headers: {
-//      Accept: "application/json",
-//      "Content-Type": "application/json",
-//    },
-//  };
+    const [ authState, setAuthState] = useAuthStore()
 
-//  const checkAuthenticated = async () =>{
-//      console.log('checking auth')
-//     const res = await axios.get("http://localhost:5000/api/authtoken", config)
+ const config = {
+   headers: {
+     Accept: "application/json",
+     "Content-Type": "application/json",
+   },
+ };
 
-//     if(res){
-//         console.log(res)
-//     }else{
-//         console.log("it don't work")
-//     }
-//  }
-// useEffect(()=>{
-//     (async ()=>{ return await checkAuthenticated()})()
+ const checkAuthenticated = async () =>{
+    const res = await axios.get("http://localhost:5000/api/authtoken", config)
+
+   try{
+    if(res.status === 200){
+        console.log(res)
+        setAuthState({
+            token:res.data.access_token,
+            isAuthenticated:true
+        })
+    
+    }else{
+        console.log(res.status)
+    }  
+   }catch(error){
+       console.log(error)
+   }
+ }
+useEffect(()=>{
+    (async ()=>{ return await checkAuthenticated()})()
    
-// })
+    console.log(authState)
+}, [])
     
 
-  return (
-
-    <div>{children}</div>
-  )
+  return (<div>{children}</div>) 
+    
+  
 }
 
 export default Layout
---
+++ client/src/utility/GlobalState.js
@@ -0,0 +1,17 @@
+import React, {useState,useContext, createContext} from 'react';
+
+    export const AuthContext = createContext();
+
+    export const AuthProvider = props =>{
+        const [authState, setAuthState] = useState({
+            token:'',
+            isAuthenticated:false
+        })
+
+
+        return(
+            <AuthContext.Provider value={[authState, setAuthState]} >{props.children}</AuthContext.Provider>
+        )
+    }
+
+    export const useAuthStore = () => useContext(AuthContext);
