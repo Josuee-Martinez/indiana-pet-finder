be694ca0b50c87b1a3a20c739ab27425b5c25c64
codesmitty <varcodesmitty@gmail.com>
2022-02-20 08:05:28 -0500

    working on form response

--- client/src/components/forms/Form.js
+++ client/src/components/forms/Form.js
@@ -1,83 +1,80 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
-import { useAuthStore } from '../../utility/GlobalState';
-import axios from 'axios'
-import Cookies from 'js-cookie'
+import { useAuthStore } from "../../utility/GlobalState";
+import axios from "axios";
+import Cookies from "js-cookie";
 
-const FormSearch = (e) =>{
-  const [authState, setAutState] = useAuthStore()
+const FormSearch = (e) => {
+  const [authState, setAutState] = useAuthStore();
   const [formValue, setFormValue] = useState({
-    type:'',
-    location:''
-  })
+    type: "",
+    location: "",
+  });
 
-
-
-  
-
-  const handleSubmit = (e,data) =>{
+  const handleSubmit = (e, data) => {
     const token = localStorage.getItem("token");
-  
+
     const config = {
       method: "GET",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`,
-        
+        Authorization: `Bearer ${token}`,
       },
-      body:JSON.stringify(data)
-      
     };
     e.preventDefault();
-    
+
     const testData = {
-      type:data?.type,
-      location:data?.location
-    }
+      type: data?.type,
+      location: data?.location,
+    };
 
-   
-   const res =  axios("http://localhost:5000/api/petdata",config ).then(res =>{
-     console.log(res)
-   });
+    const body = JSON.stringify({ type: data?.type, location: data?.location });
+    console.log(body);
 
-  }
+    const res = axios
+      .get("http://localhost:5000/api/petdata", config, body)
+      .then((res) => {
+        console.log(res);
+      });
+  };
 
-  const handleChange = (e) =>{
-    
-    const {name, value} = e.target;
-    setFormValue({...formValue, [name]:value})
-      
-  }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValue({ ...formValue, [name]: value });
+  };
 
   return (
-      <Form onSubmit={(e)=>handleSubmit(e, formValue)} className=" mx-auto my-5 mb-3" style={{ width: "35%" }}>
-        <Form.Group className="mb-2 p-4">
-          <Form.Label>Cat or Dog</Form.Label>
-          <Form.Control
-            onChange={handleChange}
-            type="text"
-            placeholder="type Cat or Dog"
-            value={formValue.type}
-            name="type"
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-2 p-4">
-          <Form.Label>Zipcode</Form.Label>
-          <Form.Control
-            onChange={handleChange}
-            type="number"
-            placeholder="Enter Zipcode"
-            value={formValue.location}
-            name="location"
-            required
-          />
-        </Form.Group>
-      <button onSubmit={(e)=>handleSubmit(e, formValue)}>submit</button>
-      </Form>
-     
+    <Form
+      onSubmit={(e) => handleSubmit(e, formValue)}
+      className=" mx-auto my-5 mb-3"
+      style={{ width: "35%" }}
+    >
+      <Form.Group className="mb-2 p-4">
+        <Form.Label>Cat or Dog</Form.Label>
+        <Form.Control
+          onChange={handleChange}
+          type="text"
+          placeholder="type Cat or Dog"
+          value={formValue.type}
+          name="type"
+          required
+        />
+      </Form.Group>
+      <Form.Group className="mb-2 p-4">
+        <Form.Label>Zipcode</Form.Label>
+        <Form.Control
+          onChange={handleChange}
+          type="number"
+          placeholder="Enter Zipcode"
+          value={formValue.location}
+          name="location"
+          required
+        />
+      </Form.Group>
+      <button onSubmit={(e) => handleSubmit(e, formValue)}>submit</button>
+    </Form>
   );
-}
+};
 
-export default FormSearch
+export default FormSearch;
--- client/src/hoc/Layout.js
+++ client/src/hoc/Layout.js
@@ -1,53 +1,50 @@
-import React, {useEffect, useState} from 'react'
-import useFetch from 'react-fetch-hook';
-import axios from 'axios'
-import {useAuthStore} from '../utility/GlobalState'
+import React, { useEffect, useState } from "react";
+import useFetch from "react-fetch-hook";
+import axios from "axios";
+import { useAuthStore } from "../utility/GlobalState";
 
-function Layout({children}) {
-    const [ authState, setAuthState] = useAuthStore()
-    const [isLoading, setIsLoading] = useState(true)
- const config = {
-   headers: {
-     Accept: "application/json",
-     "Content-Type": "application/json",
-   },
- };
+function Layout({ children }) {
+  const [authState, setAuthState] = useAuthStore();
+  const [isLoading, setIsLoading] = useState(true);
+  const config = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  };
 
- const checkAuthenticated = async () =>{
-    const res = await axios.get("http://localhost:5000/api/authtoken", config)
+  const checkAuthenticated = async () => {
+    const res = await axios.get("http://localhost:5000/api/authtoken", config);
 
-   try{
-    if(res.status === 200){
-        console.log(res)
+    try {
+      if (res.status === 200) {
+        console.log(res);
         setAuthState({
-            token:res.data.access_token,
-            isAuthenticated:true
-        })
+          token: res.data.access_token,
+          isAuthenticated: true,
+        });
 
-        localStorage.setItem('token', res?.data?.access_token)
-        setIsLoading(false)
-    }else{
-        console.log(res.status)
-    }  
-   }catch(error){
-       console.log(error)
-   }
- } 
- 
- 
-
-useEffect(()=>{
-    (async ()=>{ return await checkAuthenticated()})()
-   
-}, [])
-    
-   if (isLoading){
-        return <h1>Loading...</h1>
+        localStorage.setItem("token", res?.data?.access_token);
+        setIsLoading(false);
+      } else {
+        console.log(res.status);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-  return (<div>{children}</div>) 
-    
-  
+  useEffect(() => {
+    (async () => {
+      return await checkAuthenticated();
+    })();
+  }, []);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return <div>{children}</div>;
 }
 
-export default Layout
+export default Layout;
